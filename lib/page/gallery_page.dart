49b45f319c99f8b8f533cff817cdf7eda57cb61e
yoshis777 <32704339+yoshis777@users.noreply.github.com>
2022-10-22 03:41:19 +0900

    closeボタン押下時の検索ワードの連携

--- lib/page/gallery_page.dart
+++ lib/page/gallery_page.dart
@@ -55,126 +55,132 @@ class _GalleryPageState extends State<GalleryPage> {
   Widget build(BuildContext context) {
     ScrollController scController = ScrollController();
 
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-          actions: [
-            IconButton(
-              onPressed: () async {
-                if (promptList.isNotEmpty) {
-                  scController.animateTo(
-                      0,
-                      duration: const Duration(seconds: 1), //移動するのに要する時間を設定
-                      curve: Curves.easeOutQuint //アニメーションの種類を設定
-                  );
-                }
-              },
-              icon: const Icon(Icons.arrow_upward),
-            ),
-            IconButton(
-              onPressed: () async {
-                if (promptList.isNotEmpty) {
-                  scController.animateTo(
-                      scController.position.maxScrollExtent,
-                      duration: const Duration(seconds: 1), //移動するのに要する時間を設定
-                      curve: Curves.easeOutQuint //アニメーションの種類を設定
-                  );
-                }
-              },
-              icon: const Icon(Icons.arrow_downward),
-            ),
-            const SizedBox(width: 12),
-            Padding(
-              padding: const EdgeInsets.only(top: 6, left: 4, right: 4),
-              child: SizedBox(width: 200,
-                child: TextField(
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    alignLabelWithHint: true,
-                    labelText: "search",
-                    filled: true,
-                    fillColor: Colors.white,
-                    isDense: true,
+    return WillPopScope(
+      onWillPop: () {
+        Navigator.pop(context, [null, promptSearchTextController.text]);
+        return Future.value(false);
+      },
+      child: Scaffold(
+          appBar: AppBar(
+            title: Text(widget.title),
+            actions: [
+              IconButton(
+                onPressed: () async {
+                  if (promptList.isNotEmpty) {
+                    scController.animateTo(
+                        0,
+                        duration: const Duration(seconds: 1), //移動するのに要する時間を設定
+                        curve: Curves.easeOutQuint //アニメーションの種類を設定
+                    );
+                  }
+                },
+                icon: const Icon(Icons.arrow_upward),
+              ),
+              IconButton(
+                onPressed: () async {
+                  if (promptList.isNotEmpty) {
+                    scController.animateTo(
+                        scController.position.maxScrollExtent,
+                        duration: const Duration(seconds: 1), //移動するのに要する時間を設定
+                        curve: Curves.easeOutQuint //アニメーションの種類を設定
+                    );
+                  }
+                },
+                icon: const Icon(Icons.arrow_downward),
+              ),
+              const SizedBox(width: 12),
+              Padding(
+                padding: const EdgeInsets.only(top: 6, left: 4, right: 4),
+                child: SizedBox(width: 200,
+                  child: TextField(
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      alignLabelWithHint: true,
+                      labelText: "search",
+                      filled: true,
+                      fillColor: Colors.white,
+                      isDense: true,
+                    ),
+                    maxLines: 1,
+                    controller: promptSearchTextController,
+                    onChanged: (value) async {
+                      final searchWords = value.split(',');
+                      final repository = await PromptRepository.getInstance();
+                      repository.showSearchedPrompts(searchWords);
+                    },
                   ),
-                  maxLines: 1,
-                  controller: promptSearchTextController,
-                  onChanged: (value) async {
-                    final searchWords = value.split(',');
-                    final repository = await PromptRepository.getInstance();
-                    repository.showSearchedPrompts(searchWords);
-                  },
                 ),
               ),
-            ),
-          ],
-        ),
-        body: Container(
-          width: double.infinity,
-          height: double.infinity,
-          alignment: Alignment.topCenter,
-          child: Padding(
-            padding: const EdgeInsets.only(top:8),
-            child: SingleChildScrollView(
-              controller: scController,
-              child: FutureBuilder(
-                future: loadPromptFromDB(),
-                builder: ((context, snapshot) {
-                  if (snapshot.hasData) {
-                    if (snapshot.data!) {
-                      if (promptList.isNotEmpty) {
-                        return Wrap(
-                          children: promptList.asMap().entries.map<Widget>((prompt) {
-                            return Padding(
-                              padding: const EdgeInsets.only(bottom: 6, left: 4, right: 4),
-                              child: Container(
-                                  width: 280, height:280,
-                                  alignment: Alignment.center,
-                                  child: FutureBuilder(
-                                    future: DBUtil.getImageFullPath(prompt.value.imageData!.imagePath),
-                                    builder: (context, snapshot) {
-                                      if (snapshot.hasData) {
-                                        if (!snapshot.hasError) {
-                                          return GestureDetector(
-                                            onTap: () {
-                                              Navigator.pop(context, [prompt.key, promptSearchTextController.text]);
-                                            },
-                                            child: Image.file(File(snapshot.data!)),
-                                          );
-                                        } else {
-                                          return Text("${ErrorMessage.someError}: ${snapshot.error}");
-                                        }
-                                      } else {
-                                        return const Text(ErrorMessage.imageNotFound);
-                                      }
-                                    },
-                                  )
-                              ),
+            ],
+          ),
+          body: Container(
+              width: double.infinity,
+              height: double.infinity,
+              alignment: Alignment.topCenter,
+              child: Padding(
+                padding: const EdgeInsets.only(top:8),
+                child: SingleChildScrollView(
+                  controller: scController,
+                  child: FutureBuilder(
+                    future: loadPromptFromDB(),
+                    builder: ((context, snapshot) {
+                      if (snapshot.hasData) {
+                        if (snapshot.data!) {
+                          if (promptList.isNotEmpty) {
+                            return Wrap(
+                              children: promptList.asMap().entries.map<Widget>((prompt) {
+                                return Padding(
+                                  padding: const EdgeInsets.only(bottom: 6, left: 4, right: 4),
+                                  child: Container(
+                                      width: 280, height:280,
+                                      alignment: Alignment.center,
+                                      child: FutureBuilder(
+                                        future: DBUtil.getImageFullPath(prompt.value.imageData!.imagePath),
+                                        builder: (context, snapshot) {
+                                          if (snapshot.hasData) {
+                                            if (!snapshot.hasError) {
+                                              return GestureDetector(
+                                                onTap: () {
+                                                  Navigator.pop(context, [prompt.key, promptSearchTextController.text]);
+                                                },
+                                                child: Image.file(File(snapshot.data!)),
+                                              );
+                                            } else {
+                                              return Text("${ErrorMessage.someError}: ${snapshot.error}");
+                                            }
+                                          } else {
+                                            return const Text(ErrorMessage.imageNotFound);
+                                          }
+                                        },
+                                      )
+                                  ),
+                                );
+                              }).toList(),
                             );
-                          }).toList(),
-                        );
+                          } else {
+                            return const SizedBox(
+                                width: 600,
+                                height: 600,
+                                child: Align(
+                                  alignment: Alignment.center,
+                                  child: Text(GuidanceMessage.galleryIsEmpty,
+                                    style: TextStyle(color: Colors.grey, letterSpacing: 2),
+                                  ),
+                                )
+                            );
+                          }
+                        } else {
+                          return const Text(ErrorMessage.dbReadingError);
+                        }
                       } else {
-                        return const SizedBox(
-                            width: 600,
-                            height: 600,
-                            child: Align(
-                              alignment: Alignment.center,
-                              child: Text(GuidanceMessage.galleryIsEmpty,
-                                style: TextStyle(color: Colors.grey, letterSpacing: 2),
-                              ),
-                            )
-                        );
+                        return const Text(StateMessage.dbReading);
                       }
-                    } else {
-                      return const Text(ErrorMessage.dbReadingError);
-                    }
-                  } else {
-                    return const Text(StateMessage.dbReading);
-                  }
-                }),
-              ),
-            ),
+                    }),
+                  ),
+                ),
+              )
           )
-        )
+      ),
     );
   }
 }
