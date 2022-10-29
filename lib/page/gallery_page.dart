ca129eb7e452cfde20042cedbb94bb715a92c6b6
yoshis777 <32704339+yoshis777@users.noreply.github.com>
2022-10-29 17:04:34 +0900

    リファクタリング

--- lib/page/gallery_page.dart
+++ lib/page/gallery_page.dart
@@ -1,13 +1,10 @@
-import 'dart:io';
-
+import 'package:ai_prompt_organizer/component/build_gallary_prompt_list.dart';
 import 'package:ai_prompt_organizer/domain/prompt.dart';
 import 'package:flutter/material.dart';
 
-import '../ai_prompt_organizer.dart';
 import '../model/schema/prompt.dart';
 import '../repository/prompt_repository.dart';
-import '../util/db_util.dart';
-import 'full_screen_dialog_page.dart';
+
 
 class GalleryPage extends StatefulWidget {
   const GalleryPage({super.key, this.searchWord});
@@ -131,84 +128,11 @@ class _GalleryPageState extends State<GalleryPage> {
               alignment: Alignment.topCenter,
               child: Padding(
                 padding: const EdgeInsets.only(top:8),
-                child: SingleChildScrollView(
-                  controller: scController,
-                  child: FutureBuilder(
-                    future: loadPromptFromDB(),
-                    builder: ((context, snapshot) {
-                      if (snapshot.hasData) {
-                        if (snapshot.data!) {
-                          if (promptList.isNotEmpty) {
-                            return Wrap(
-                              children: promptList.asMap().entries.map<Widget>((prompt) {
-                                return Padding(
-                                  padding: const EdgeInsets.only(bottom: 6, left: 4, right: 4),
-                                  child: Container(
-                                      width: 280, height:280,
-                                      alignment: Alignment.center,
-                                      child: FutureBuilder(
-                                        future: DBUtil.getImageFullPath(prompt.value.imageData!.imagePath),
-                                        builder: (context, snapshot) {
-                                          if (snapshot.hasData) {
-                                            if (!snapshot.hasError) {
-                                              return Stack(
-                                                alignment: Alignment.bottomRight,
-                                                children: [
-                                                  GestureDetector(
-                                                    onTap: () {
-                                                      if (prompt.value.imageData?.imagePath != null) {
-                                                        Navigator.push(
-                                                          context,
-                                                          MaterialPageRoute(
-                                                            builder: (_) => FullScreenDialogPage(imagePath: prompt.value.imageData!.imagePath),
-                                                            fullscreenDialog: true,
-                                                          ),
-                                                        );
-                                                      }
-                                                    },
-                                                    child: Image.file(File(snapshot.data!)),
-                                                  ),
-                                                  IconButton(
-                                                    onPressed: () {
-                                                      Navigator.pop(context, [prompt.key, promptSearchTextController.text]);
-                                                    },
-                                                    icon: const Icon(Icons.arrow_right, color: Colors.grey)
-                                                  )
-                                                ],
-                                              );
-                                            } else {
-                                              return Text("${ErrorMessage.someError}: ${snapshot.error}");
-                                            }
-                                          } else {
-                                            return const Text(ErrorMessage.imageNotFound);
-                                          }
-                                        },
-                                      )
-                                  ),
-                                );
-                              }).toList(),
-                            );
-                          } else {
-                            return const SizedBox(
-                                width: 600,
-                                height: 600,
-                                child: Align(
-                                  alignment: Alignment.center,
-                                  child: Text(GuidanceMessage.galleryIsEmpty,
-                                    style: TextStyle(color: Colors.grey, letterSpacing: 2),
-                                  ),
-                                )
-                            );
-                          }
-                        } else {
-                          return const Text(ErrorMessage.dbReadingError);
-                        }
-                      } else {
-                        return const Text(StateMessage.dbReading);
-                      }
-                    }),
-                  ),
-                ),
+                child: buildGallaryPromptList(
+                    scController: scController,
+                    promptList: promptList,
+                    promptSearchTextController: promptSearchTextController,
+                    loadPromptFromDB: loadPromptFromDB)
               )
           )
       ),
